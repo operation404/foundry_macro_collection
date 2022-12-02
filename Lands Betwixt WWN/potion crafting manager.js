8c33e6f8a932b1fa8b4541ba8074710b186a2dda
operation404 <operation404@gmail.com>
2022-12-02 11:43:58 -0800

    Updated with a preset list of potions that will fill out the necessary data into the form.

--- Lands Betwixt WWN/potion crafting manager.js
+++ Lands Betwixt WWN/potion crafting manager.js
@@ -4,11 +4,82 @@ const alchemy_skill_item = josh_actor.data.items.find(i => i.data.type === "skil
 const potion_crafting = josh_actor.data.data.attributes.potion_crafting;
 const alchemy_formula = "@alchemy.skillDice + @alchemy.ownedLevel + @scores.int.mod";
 
+const potion_presets = {
+    // Core book magic elixirs
+    "Anchoring Draught":        {"dc" : 9,  "cost": 500,   "time": 7},
+    "Bestial Form":             {"dc" : 10, "cost": 1250,  "time": 7},
+    "Blood of Boiling Rage":    {"dc" : 8,  "cost": 250,   "time": 7},
+    "Bodily Innervation":       {"dc" : 11, "cost": 2500,  "time": 7},
+    "Borrowed Flesh":           {"dc" : 8,  "cost": 500,   "time": 7},
+    "Cold Courage":             {"dc" : 9,  "cost": 1000,  "time": 7},
+    "Congealed Winter":         {"dc" : 9,  "cost": 1250,  "time": 7},
+    "Deep Sight":               {"dc" : 10, "cost": 1250,  "time": 7},
+    "Energetic Impermeability": {"dc" : 9,  "cost": 500,   "time": 7},
+    "Inverted Entropy":         {"dc" : 10, "cost": 1250,  "time": 7},
+    "Murderous Anointing":      {"dc" : 9,  "cost": 500,   "time": 7},
+    "Nectar of Immortality":    {"dc" : 12, "cost": 50000, "time": 30},
+    "Nepenthe":                 {"dc" : 11, "cost": 12500, "time": 30},
+    "Omened Visions":           {"dc" : 8,  "cost": 375,   "time": 7},
+    "Persistent Luminescence":  {"dc" : 8,  "cost": 250,   "time": 7},
+    "Plasmic Molding":          {"dc" : 9,  "cost": 500,   "time": 7},
+    "Quintessence of the Hour": {"dc" : 10, "cost": 2500,  "time": 7},
+    "Sacrificial Strength":     {"dc" : 9,  "cost": 500,   "time": 7},
+    "Sanctified Healing":       {"dc" : 8,  "cost": 250,   "time": 7},
+    "Scalding Breath":          {"dc" : 9,  "cost": 500,   "time": 7},
+    "Soul Sight":               {"dc" : 8,  "cost": 250,   "time": 7},
+    "Thaumic Vitality":         {"dc" : 11, "cost": 2500,  "time": 7},
+    "The Great and the Small":  {"dc" : 10, "cost": 500,   "time": 7},
+    "Truthspeaking":            {"dc" : 10, "cost": 1250,  "time": 7},
+    "Wrathful Detonation":      {"dc" : 9,  "cost": 250,   "time": 7},
+    
+    // Atlas of latter earth lesser works
+    "Acid Vial":            {"dc" : 10, "cost": 50,  "time": 1},
+    "Binding Paste":        {"dc" : 9,  "cost": 50,  "time": 1},
+    "Blinding Dust":        {"dc" : 9,  "cost": 50,  "time": 2},
+    "Healing Salve":        {"dc" : 7,  "cost": 10,  "time": 1},
+    "Incandescent Dust":    {"dc" : 9,  "cost": 500, "time": 1},
+    "Inflammable Oil":      {"dc" : 8,  "cost": 100, "time": 1},
+    "Itching Dust":         {"dc" : 9,  "cost": 100, "time": 2},
+    "Luminescent Oil":      {"dc" : 8,  "cost": 50,  "time": 1},
+    "Self-Igniting Oil":    {"dc" : 8,  "cost": 50,  "time": 3},
+    "Smoke Cloud":          {"dc" : 9,  "cost": 25,  "time": 2},
+    "Soothing Powder":      {"dc" : 8,  "cost": 25,  "time": 2},
+    "Touchstone":           {"dc" : 10, "cost": 50,  "time": 3},
+
+    // Atlas of latter earth greater works
+    "Aqueous Solvent":      {"dc" : 11, "cost": 250,  "time": 5},
+    "Courage Elixir":       {"dc" : 10, "cost": 200,  "time": 7},
+    "Infernal Wind":        {"dc" : 11, "cost": 250,  "time": 7},
+    "Lethal Poison":        {"dc" : 11, "cost": 500,  "time": 7},
+    "Revivifying Draught":  {"dc" : 10, "cost": 200,  "time": 7},
+    "Sleeping Powders":     {"dc" : 10, "cost": 200,  "time": 3},
+    "Sovereign Specific":   {"dc" : 11, "cost": 200,  "time": 7},
+    "Unquenchable Flame":   {"dc" : 12, "cost": 1000, "time": 14},
+};
+
+const potion_presets_list_items_template = `
+        {{#each this}}
+            <option value="{{@key}}">{{@key}}</option>
+        {{/each}}
+    `;
+const rendered_template = (Handlebars.compile(potion_presets_list_items_template))(potion_presets);
+
 const custom_dialog = new Dialog({
     title:`Potion Crafting Manager`,
     
     content: `
         <form>
+            <div class="form-group" style="flex-direction: row;">
+                <div></div>
+                <label style="white-space: nowrap; flex-grow: 0;padding-right: 10px;">Select preset (clears fields!): </label>
+                <select style="text-align:left; height: auto; min-width: 190px; flex-grow: 0;" id="potion_presets">
+                    <option disabled selected value> -- select an option -- </option>
+                    ${rendered_template}
+                </select>
+                <div></div>
+            </div>
+            <hr>
+            
             <div class="form-group" style="flex-direction: row;">
                 <div></div>
                 <label style="white-space: nowrap; flex-grow: 0; padding-right: 10px;">Potion name: </label>
@@ -22,7 +93,6 @@ const custom_dialog = new Dialog({
                     <option value="2" ${potion_crafting.batch_size.value === 2 ? selected="selected" : ``}>2</option>
                     <option value="4" ${potion_crafting.batch_size.value === 4 ? selected="selected" : ``}>4</option>
                 </select>
-                
                 <div></div>
             </div>
             
@@ -35,7 +105,7 @@ const custom_dialog = new Dialog({
                 <input style="text-align:center; height: auto; min-width: 80px; flex-grow: 0;" id="modifier" type="number" step="1" value="${potion_crafting.modifier.value}" />
                 <div></div>
                 <label style="white-space: nowrap; flex-grow: 0; padding-right: 5px;">Rushed? </label>
-                <input type="checkbox" id="rushed" ${potion_crafting.rushed.value ? `checked` : ``}>
+                <input type="checkbox" id="rushed" title="While rushing, 2 days of progress are made for every 1 real day, and a skill check is made at the end to determine if the potion is crafted." ${potion_crafting.rushed.value ? `checked` : ``}>
                 <div></div>
             </div>
             <hr>
@@ -87,7 +157,7 @@ const custom_dialog = new Dialog({
             clear_fields();
         });
         document.getElementById("progress_button").addEventListener("click", (e) => {
-            const progressed_days = parseInt(document.getElementById("days_to_progress").value);
+            const progressed_days = parseInt(document.getElementById("days_to_progress").value) * (document.getElementById("rushed").checked ? 2 : 1);
             const days_left = parseInt(document.getElementById("days_remaining").value) - progressed_days;
             if (days_left > 0) {
                 document.getElementById("days_to_progress").value = 0;
@@ -132,6 +202,13 @@ const custom_dialog = new Dialog({
                     });
                 }
             }
+        });
+        document.getElementById("potion_presets").addEventListener("change", (e) => {
+            clear_fields();
+            const selected_potion = potion_presets[document.getElementById("potion_presets").value];
+            document.getElementById("potion_name").value = document.getElementById("potion_presets").value;
+            document.getElementById("craft_dc").value = selected_potion.dc;
+            document.getElementById("days_remaining").value = selected_potion.time;
         });
     }, 
     
