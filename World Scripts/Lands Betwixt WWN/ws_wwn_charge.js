869324b3888bdbc3a167d2c9b862d0cce225c550
operation404 <operation404@gmail.com>
2022-12-04 22:45:19 -0800

    Converted to class with static properties, moved ac management to hooks on active effect creation and deletion, added charge to the effects list on startup.

--- World Scripts/Lands Betwixt WWN/ws_wwn_charge.js
+++ World Scripts/Lands Betwixt WWN/ws_wwn_charge.js
@@ -1,131 +1,107 @@
-// This script was written to deal with CUB conditions, but I wanted
-// to know how to actually manage the conditions myself instead of
-// just using CUB helper functions.
+class WWN_Charge {
 
-// Freeze to prevent the changing or deleting the functions
-const WWN_Charge = Object.freeze({
-
-    // tokens should be an array or iterable
-    // considered checking for array instance, but unsure if that's good practice
-    apply: (tokens) => {
-        const condition_name = "EFFECT.Charge";
-        const charge_ae_condition_data = game.settings.get("combat-utility-belt", "activeConditionMap").find(c => c.name === condition_name);
-        if (charge_ae_condition_data === undefined) {
-            ui.notifications.error(`Condition '${condition_name}' not found.`);
-            return;
-        }
-
-        const charge_ae_condition = {
-            // WWN system doesn't seem to support active effects. Have to do it manually
-            //"changes": [{ "key": "data.aac.mod", "value": "-2", "mode": 2, "priority": 21 }],
-            "flags": {
-                "combat-utility-belt": {
-                    "conditionId": charge_ae_condition_data.id,
-                    "overlay": false
-                },
-                "core": {
-                    "overlay": false,
-                    "statusId": "combat-utility-belt." + charge_ae_condition_data.id
-                }
-            },
-            "icon": charge_ae_condition_data.icon,
-            //"id": "combat-utility-belt.charge", // Not actually sure what this is for
-            "label": charge_ae_condition_data.name // Same as 'condition_name'
-        };
-
-        //tokens = tokens instanceof Array ? tokens : [tokens];
-        tokens.forEach(t => {
-            if (t.actor.effects.find(c => c.data.flags.core.statusId === charge_ae_condition.flags.core.statusId) === undefined) {
-                t.actor.createEmbeddedDocuments("ActiveEffect", [charge_ae_condition]);
-                if (t.actor.type === "character") {
-                    t.actor.update({
-                        "data.aac.mod": t.actor.data.data.aac.mod - 2,
-                    });
-                } else if (t.actor.type === "monster") {
-                    // monster sheets don't utilize aac.mod
-                    t.actor.update({
-                        "data.aac.value": t.actor.data.data.aac.value - 2,
-                    });
-                }
-                ChatMessage.create({
-                    user: game.user._id,
-                    speaker: ChatMessage.getSpeaker({
-                        actor: t.actor
-                    }),
-                    content: `<span>${t.actor.name} <b style="color:blue;">charges</b> forward!</span><br>`
-                });
+    static condition_data = {
+        "flags": {
+            "core": {
+                "statusId": "WWN_Charge_Condition",
+                "overlay": false
             }
+        },
+        "icon": "icons/charging-bull.svg",
+        "label": "Charge"
+    };
+    static adjust_values = {
+        "add": +2,
+        "subtract": -2
+    };
+    static adjust_keys = {
+        "character": "mod",
+        "monster": "value"
+    };
+
+    static init() {
+        WWN_Charge.prepare_hook_handlers();
+
+        // Create a global object to expose only the desired functions
+        window.WWN_Charge = {
+            apply: WWN_Charge.apply,
+            remove: WWN_Charge.remove
+        };
+        console.log("=== WWN charge world script loaded ===");
+    }
+
+    static prepare_hook_handlers() {
+        Hooks.once("setup", WWN_Charge.create_charge_status_effect);
+        Hooks.on("createActiveEffect", WWN_Charge.apply_ac_penalty);
+        Hooks.on("deleteActiveEffect", WWN_Charge.remove_ac_penalty);
+        Hooks.on("updateCombat", WWN_Charge.turn_start_remove_handler);
+        Hooks.on("deleteCombat", WWN_Charge.combat_end_remove_handler);
+    }
+
+    static create_charge_status_effect() {
+        CONFIG.statusEffects.push({
+            id: WWN_Charge.condition_data.flags.core.statusId,
+            label: WWN_Charge.condition_data.label,
+            icon: WWN_Charge.condition_data.icon
         });
-    },
+    }
 
-    // tokens should be an array or iterable
-    remove: (tokens) => {
-        const condition_name = "EFFECT.Charge";
-        const charge_ae_condition_data = game.settings.get("combat-utility-belt", "activeConditionMap").find(c => c.name === condition_name);
-        if (charge_ae_condition_data === undefined) {
-            ui.notifications.error(`Condition '${condition_name}' not found.`);
-            return;
-        }
+    static apply_ac_penalty(active_effect_document, options, user_id) {
+        if (active_effect_document.data.flags.core.statusId !== WWN_Charge.condition_data.flags.core.statusId) return;
+        WWN_Charge.adjust_ac(active_effect_document.parent, "subtract");
+    }
 
+    static remove_ac_penalty(active_effect_document, options, user_id) {
+        if (active_effect_document.data.flags.core.statusId !== WWN_Charge.condition_data.flags.core.statusId) return;
+        WWN_Charge.adjust_ac(active_effect_document.parent, "add");
+    }
+
+    static turn_start_remove_handler(combat_document, change, options, user_id) {
+        if (!combat_document.started) return;
+
+        // Only run on one GM client if multiple connected
+        const active_gm_user = game.users.find(u => u.isGM && u.active);
+        if (active_gm_user === undefined || active_gm_user.id !== game.user.id) return;
+
+        const acting_token = canvas.tokens.documentCollection.find(t => t.id === combat_document.current.tokenId);
+        if (acting_token !== undefined) WWN_Charge.remove([acting_token]);
+    }
+
+    static combat_end_remove_handler(combat_document, options, user_id) {
+        // Only GM can end combat so user_id is guaranteed GM, runs on only their client
+        if (user_id !== game.user.id) return;
+
+        const combat_scene_tokens = combat_document.scene.tokens;
+        if (combat_scene_tokens !== undefined) WWN_Charge.remove(Array.from(combat_scene_tokens));
+    }
+
+    static adjust_ac(actor, adjust_type) {
+        if (WWN_Charge.adjust_keys[actor.type] === undefined) throw new Error(`Invalid actor type: ${actor.type}`);
+        actor.update({
+            // { [VAR]: ... } syntax allows using dynamic keys in object literals
+            ["data.aac." + WWN_Charge.adjust_keys[actor.type]]: actor.data.data.aac[WWN_Charge.adjust_keys[actor.type]] + WWN_Charge.adjust_values[adjust_type]
+        });
+    }
+
+    static apply(tokens) {
         tokens = tokens instanceof Array ? tokens : [tokens];
         tokens.forEach(t => {
-            const t_charge_cond = t.actor.effects.find(c => c.data.flags.core.statusId === `combat-utility-belt.${charge_ae_condition_data.id}`);
-            if (t_charge_cond !== undefined) {
-                t.actor.deleteEmbeddedDocuments("ActiveEffect", [t_charge_cond.id]);
-                if (t.actor.type === "character") {
-                    t.actor.update({
-                        "data.aac.mod": t.actor.data.data.aac.mod + 2,
-                    });
-                } else if (t.actor.type === "monster") {
-                    // monster sheets don't utilize aac.mod
-                    t.actor.update({
-                        "data.aac.value": t.actor.data.data.aac.value + 2,
-                    });
-                }
-                ChatMessage.create({
-                    user: game.user._id,
-                    speaker: ChatMessage.getSpeaker({
-                        actor: t.actor
-                    }),
-                    content: `<span>${t.actor.name} recovers from <b style="color:blue;">charging</b>.</span><br>`
-                });
+            if (t.actor.effects.find(c => c.data.flags.core.statusId === WWN_Charge.condition_data.flags.core.statusId) === undefined) {
+                t.actor.createEmbeddedDocuments("ActiveEffect", [WWN_Charge.condition_data]);
             }
         });
-    },
+    }
 
-});
+    static remove(tokens) {
+        tokens = tokens instanceof Array ? tokens : [tokens];
+        tokens.forEach(t => {
+            const t_charge_cond = t.actor.effects.find(c => c.data.flags.core.statusId === WWN_Charge.condition_data.flags.core.statusId);
+            if (t_charge_cond !== undefined) {
+                t.actor.deleteEmbeddedDocuments("ActiveEffect", [t_charge_cond.id]);
+            }
+        });
+    }
 
-// 'combatTurn' - hook for combat turn changing, not in v9 it seems
-// 'combatRound' exists too
-// updateCombat does the job fine too
+}
 
-// Remove the charge effect from the current acting token in turn order
-Hooks.on("updateCombat", (combat_document, change, options, userId) => {
-    if (!combat_document.started) return;
-    const active_gm_user = game.users.find(u => u.isGM && u.active);
-    if (active_gm_user === undefined || active_gm_user.id !== game.user.id) return;
-    const acting_token = canvas.tokens.documentCollection.find(t => t.id === combat_document.current.tokenId);
-    if (acting_token !== undefined) WWN_Charge.remove([acting_token]);
-});
-
-// Remove charge effect from all tokens after the combat ends
-Hooks.on("deleteCombat", (combat_document, options, userId) => {
-    //const active_gm_user = game.users.find(u => u.isGM && u.active);
-    //if (active_gm_user === undefined || active_gm_user.id !== game.user.id) return;
-
-    // 'userId' is the id of the person who caused the hook to be fired
-    // For example, if I ended the combat, it will be my id there on all clients
-    // Since only should be able to end combat, it's assumed the person firing it is a GM
-    if (userId !== game.user.id) return;
-    WWN_Charge.remove(Array.from(combat_document.scene.tokens));
-});
-
-// window.WWN_Charge = WWN_Charge;
-// Below is a stricter version that ensures the WWN_Charge property cannot be modified
-Object.defineProperty(window, 'WWN_Charge', {
-    value: WWN_Charge,
-    writable: false,
-    configurable: false
-});
-
-console.log("=== WWN Charge world script loaded. ===");
+WWN_Charge.init();
