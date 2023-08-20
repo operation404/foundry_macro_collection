04692cfbd3497e468cb77ef04220b9a68892d525
operation404 <operation404@gmail.com>
2023-08-19 22:10:09 -0700

    Clear targets after healing, enter targeting mode if no targets on macro use

--- Legacy WWN/healing touch.js
+++ Legacy WWN/healing touch.js
@@ -1,3 +1,13 @@
+/* 
+Usage:
+If targeting tokens, attempt to heal those tokens and clear targets.
+
+If not, enter targeting mode and heal next targeted token then clear targets.
+Cancel macro early if tool is changed off targeting.
+
+Tokens who aren't friendly, are at max hp, or have max system strain won't be healed.
+*/
+
 const FRIENDLY = 1;
 const brad_actor = game.actors.find((actor) => actor.data.name === 'Aldin Conger');
 const heal_data = {
@@ -6,47 +16,79 @@ const heal_data = {
 };
 const heal_roll_string = '2d6 + @heal_skill_item.data.data.ownedLevel + @details.level';
 
-game.user.targets.forEach((target) => {
-    const msg_as_target = (message) => send_message(target.document, message);
-    const target_data = {
-        ...target.actor.data.data,
-        type: target.actor.data.type,
-        disposition: target.data.disposition,
-        strain: target.actor.data.data.details.strain,
-    };
+const heal_and_clear_targets = (tokens) => {
+    heal_tokens(tokens);
+    game.user.updateTokenTargets([]);
+};
 
-    const dont_heal_msg =
-        target_data.disposition !== FRIENDLY
-            ? `Shouldn't heal unfriendly target!`
-            : target_data.hp.value >= target_data.hp.max
-            ? `Target is at full HP!`
-            : target_data.strain && target_data.strain.value >= target_data.strain.max
-            ? `Target is at max system strain!`
-            : null;
-    if (dont_heal_msg) return msg_as_target(dont_heal_msg);
+if (game.user.targets.size === 0) {
+    const token_controls = document.body.querySelector('nav#controls li.scene-control[data-control="token"]');
+    const target_control = document.body.querySelector('nav#controls li.control-tool[data-tool="target"]');
+    if (token_controls && target_control) {
+        token_controls.click();
+        target_control.click();
 
-    new Roll(heal_roll_string, heal_data).roll({ async: true }).then((heal_roll) => {
-        const changes = {
-            'data.hp.value': Math.min(target_data.hp.value + heal_roll.total, target_data.hp.max),
+        const target_hook_id = Hooks.once('targetToken', (user, token, targeted) => {
+            console.log('targetToken');
+            if (targeted) heal_and_clear_targets([token]);
+            Hooks.off('renderSceneControls', controls_hook_id);
+        });
+
+        const controls_hook_id = Hooks.on('renderSceneControls', (scene_controls, html, options) => {
+            console.log('renderSceneControls');
+            if (scene_controls.activeControl !== 'token' || scene_controls.activeTool !== 'target') {
+                Hooks.off('targetToken', target_hook_id);
+                Hooks.off('renderSceneControls', controls_hook_id);
+            }
+        });
+    }
+} else heal_and_clear_targets(game.user.targets);
+
+// -------- helper functions --------
+
+function heal_tokens(tokens) {
+    tokens.forEach((target) => {
+        const msg_as_target = (message) => send_message(target.document, message);
+        const target_data = {
+            ...target.actor.data.data,
+            type: target.actor.data.type,
+            disposition: target.data.disposition,
+            strain: target.actor.data.data.details.strain,
         };
-        let heal_msg = `<span><b style="color:green;">${target_data.hp.value}</b> hp &#8594; <b style="color:green;">${changes['data.hp.value']}</b> hp</span><br>`;
 
-        if (target_data.strain) {
-            changes['data.details.strain.value'] = target_data.strain.value + 1;
-            heal_msg = `${heal_msg}<span><b style="color:maroon;">${target_data.strain.value}</b> strain &#8594; <b style="color:maroon;">${changes['data.details.strain.value']}</b> strain</span><br>`;
-        }
+        const dont_heal_msg =
+            target_data.disposition !== FRIENDLY
+                ? `Shouldn't heal unfriendly target!`
+                : target_data.hp.value >= target_data.hp.max
+                ? `Target is at full HP!`
+                : target_data.strain && target_data.strain.value >= target_data.strain.max
+                ? `Target is at max system strain!`
+                : null;
+        if (dont_heal_msg) return msg_as_target(dont_heal_msg);
 
-        apply_healing(target, changes).then((error_msg) => {
-            if (error_msg) ui.notifications.error(err_msg);
-            else
-                heal_roll
-                    .render()
-                    .then((heal_render) =>
-                        msg_as_target(`<span>${target.name} is healed!</span><br>${heal_render}${heal_msg}`)
-                    );
+        new Roll(heal_roll_string, heal_data).roll({ async: true }).then((heal_roll) => {
+            const changes = {
+                'data.hp.value': Math.min(target_data.hp.value + heal_roll.total, target_data.hp.max),
+            };
+            let heal_msg = `<span><b style="color:green;">${target_data.hp.value}</b> hp &#8594; <b style="color:green;">${changes['data.hp.value']}</b> hp</span><br>`;
+
+            if (target_data.strain) {
+                changes['data.details.strain.value'] = target_data.strain.value + 1;
+                heal_msg = `${heal_msg}<span><b style="color:maroon;">${target_data.strain.value}</b> strain &#8594; <b style="color:maroon;">${changes['data.details.strain.value']}</b> strain</span><br>`;
+            }
+
+            apply_healing(target, changes).then((error_msg) => {
+                if (error_msg) ui.notifications.error(err_msg);
+                else
+                    heal_roll
+                        .render()
+                        .then((heal_render) =>
+                            msg_as_target(`<span>${target.name} is healed!</span><br>${heal_render}${heal_msg}`)
+                        );
+            });
         });
     });
-});
+}
 
 async function apply_healing(token, changes) {
     try {
